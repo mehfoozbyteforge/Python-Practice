797ffc03b332c76e8b6e8bc136cfc67a49bc83a6
Mehfooz <mehfooz.codeworks@gmail.com>
2026-01-31 13:16:39 +0530

    Initial commit

---
+++ basics/baics_01.py
@@ -0,0 +1,18 @@
+# Q1: Print Hello World
+print('Hello World!')
+
+# Q2: Print your name 
+print('Mehfooz')
+
+# Q3: Store your age in a variable and print it
+age = 18 
+print(age)
+
+# Q4: Take name as input and print it
+name = input('Enter your name : ')
+print(name)
+
+# Q5: Take two numbers as input and print their sum
+a = int(input('Enter first number : '))
+b = int(input('Enter second number : '))
+print(a+b)
