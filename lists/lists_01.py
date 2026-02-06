2e8840996ab45d6e1b9054fece0c634485675515
Mehfooz <mehfooz.codeworks@gmail.com>
2026-02-06 19:56:29 +0530

    started lists

---
+++ lists/lists_01.py
@@ -0,0 +1,12 @@
+# Q1: List creation, indexing and slicing
+
+nums = [10, 20, 30, 40, 50, 60]
+
+# First element
+print("First element:", nums[0])
+
+# Last element
+print("Last element:", nums[-1])
+
+# Middle elements (excluding first and last)
+print("Middle elements:", nums[1:-1])
