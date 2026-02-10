3704b9b8b99342804dfc033d1ebec17086798a2b
Mehfooz <mehfooz.codeworks@gmail.com>
2026-02-10 21:58:37 +0530

    added dictionary basics

---
+++ 6.Dictionaries/dict_01.py
@@ -0,0 +1,13 @@
+# Q1: Create a dictionary and access values using keys
+
+student = {
+    "name": "Mehfooz",
+    "age": 21,
+    "course": "BCA",
+    "city": "Kolkata"
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+print("City:", student["city"])
