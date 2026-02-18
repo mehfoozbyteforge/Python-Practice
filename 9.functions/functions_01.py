afef051081e2ffb96ee53848350b7ef7f0805eba
Mehfooz <mehfooz.codeworks@gmail.com>
2026-02-18 22:49:28 +0530

    Question practice

--- 9.functions/functions_01.py
+++ 9.functions/functions_01.py
@@ -4,3 +4,12 @@ def greet():
     print("Hello, welcome to backend journey!")
 
 greet()
+
+
+# Q2: Function with parameters
+
+def greet(name):
+    print("Hello,", name, "welcome!")
+
+greet("Mehfooz")
+greet("Ray")
