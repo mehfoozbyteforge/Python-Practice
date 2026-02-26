0792fbb468559aa639313ddbfad4d3ce0b510dba
Mehfooz <mehfooz.codeworks@gmail.com>
2026-02-26 23:59:44 +0530

    question practiced

--- 11.object oriented programming/oops_01.py
+++ 11.object oriented programming/oops_01.py
@@ -9,3 +9,22 @@ s1 = Student()
 
 # Calling method
 s1.greet()
+
+# Q2: Constructor and instance variables
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+# Creating objects
+s1 = Student("Mehfooz", 21)
+s2 = Student("Ray", 22)
+
+s1.display()
+s2.display()
