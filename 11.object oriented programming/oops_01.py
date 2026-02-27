68870360458f3073fbe085e4fff4c635d1bfc0c4
Mehfooz <mehfooz.codeworks@gmail.com>
2026-02-27 21:33:15 +0530

    Question practiced

--- 11.object oriented programming/oops_01.py
+++ 11.object oriented programming/oops_01.py
@@ -2,13 +2,14 @@
 
 class Student:
     def greet(self):
-        print("Hello, I am a student.")
+        print("Hello, I am a student.\n")
 
 # Creating object
 s1 = Student()
 
 # Calling method
 s1.greet()
+
 
 # Q2: Constructor and instance variables
 
@@ -23,8 +24,31 @@ class Student:
 
 
 # Creating objects
-s1 = Student("Mehfooz", 21)
-s2 = Student("Ray", 22)
+s1 = Student("Mehfooz", 18)
+s2 = Student("Ray\n", 22)
 
 s1.display()
 s2.display()
+
+
+# Q3: Class variable vs Instance variable
+
+class Student:
+    school_name = "ABC School"   # Class variable (common)
+
+    def __init__(self, name):
+        self.name = name         # Instance variable (unique)
+
+    def display(self):
+        print("Name:", self.name)
+        print("School:", Student.school_name)
+
+
+s1 = Student("Mehfooz")
+s2 = Student("Ray")
+
+s1.display()
+print("------")
+s2.display()
+
+
